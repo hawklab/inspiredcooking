e4facf678d0fb96e2c6493f4b52372cff0d6d271
Sebastian Gamboa <me@sagmor.com>
2020-06-25 03:55:57 +0000

    Add mongodb config

--- config/spring.rb
+++ config/spring.rb
@@ -1,7 +1,15 @@
+# Having issues with spring and our mongodb files in the tmp folder
 # Spring.watch(
 #   ".ruby-version",
 #   ".rbenv-vars",
 #   "tmp/restart.txt",
 #   "tmp/caching-dev.txt"
 # )
-# Spring.watch_ignore = %w[tmp/mongodb]
+
+# module SpringWatcherListenIgnorer
+#   def start
+#     super
+#     listener.ignore(/^tmp\/mongodb|^node_modules/)
+#   end
+# end
+# Spring::Watcher::Listen.prepend SpringWatcherListenIgnorer
