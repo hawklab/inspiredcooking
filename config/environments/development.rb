0d2e67eaba3bd2ec9242f93978654e884fe5fd5a
Sebastian Gamboa <me@sagmor.com>
2020-06-30 22:30:36 +0000

    Switch to FileUpdateChecker watcher

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,5 +44,7 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  # Changed following https://stackoverflow.com/a/36616931 as the evented reloader is causing issues on Windows+devcontainers
+  # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.file_watcher = ActiveSupport::FileUpdateChecker
 end
