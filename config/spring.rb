5cabb5a2c972d4764f2fadaf47df5c28a653a2f1
Sebastian Gamboa <me@sagmor.com>
2020-06-30 22:29:10 +0000

    Restore spring configs

--- config/spring.rb
+++ config/spring.rb
@@ -1,15 +1,6 @@
-# Having issues with spring and our mongodb files in the tmp folder
-# Spring.watch(
-#   ".ruby-version",
-#   ".rbenv-vars",
-#   "tmp/restart.txt",
-#   "tmp/caching-dev.txt"
-# )
-
-# module SpringWatcherListenIgnorer
-#   def start
-#     super
-#     listener.ignore(/^tmp\/mongodb|^node_modules/)
-#   end
-# end
-# Spring::Watcher::Listen.prepend SpringWatcherListenIgnorer
+Spring.watch(
+  ".ruby-version",
+  ".rbenv-vars",
+  "tmp/restart.txt",
+  "tmp/caching-dev.txt"
+)
