f1bbfce779fa07138c310a06c276935547fc08dd
Sebastian Gamboa <me@sagmor.com>
2020-07-05 16:25:47 +0000

    Add ingredients to seed

--- config/seed.rb
+++ config/seed.rb
@@ -3,13 +3,31 @@ Recipe.create({
   description: "lalala",
   steps: [
     {
-      instructions: "Paso 1"
+      instructions: "Paso 1",
+      ingredients: [
+        {
+          name: "Harina",
+          quantity: [500, "grams"]
+        }
+      ]
     },
     {
-      instructions: "Paso 2"
+      instructions: "Paso 2",
+      ingredients: [
+        {
+          name: "Harina",
+          quantity: [100, "grams"]
+        }
+      ]
     },
     {
-      instructions: "Paso 3"
+      instructions: "Paso 3",
+      ingredients: [
+        {
+          name: "Azucar",
+          quantity: [100, "grams"]
+        }
+      ]
     },
     {
       instructions: "Paso 4"
