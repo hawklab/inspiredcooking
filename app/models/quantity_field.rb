901b298eb241c8a8394d0315b551cb7a4a6061c9
Sebastian Gamboa <me@sagmor.com>
2020-07-06 00:36:09 +0000

    Add methods to serialize quantity fields into mongodb

--- app/models/quantity_field.rb
+++ app/models/quantity_field.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 # Class to tepresents an arbitrary quantity
-#
-# To be Stored on the database.
-# See: https://docs.mongodb.com/mongoid/7.0/tutorials/mongoid-documents/index.html#custom-fields
 class QuantityField
   attr_reader :amount, :unit
 
@@ -13,18 +10,41 @@ class QuantityField
   end
 
   def +(other)
-    if unit != other.unit
-      throw "unit mismatch"
-    end
+    throw ArgumentError.new('unit mismatch') if unit != other.unit
 
     QuantityField.new(amount + other.amount, unit)
   end
 
-  def *(factor)
-    QuantityField.new(amount * factor, unit)
+  def *(other)
+    QuantityField.new(amount * other, unit)
   end
 
   def ==(other)
     amount == other.amount && unit == other.unit
+  end
+
+  # MEthods to stored on the database.
+  # See: https://docs.mongodb.com/mongoid/7.0/tutorials/mongoid-documents/index.html#custom-fields
+
+  # Converts an object of this instance into a database friendly value.
+  def mongoize
+    [amount, unit]
+  end
+
+  class << self
+    # Get the object as it was stored in the database, and instantiate
+    # this custom class from it.
+    def demongoize(object)
+      QuantityField.new(object[0], object[1])
+    end
+
+    # Takes any possible object and converts it to how it would be
+    # stored in the database.
+    def mongoize(object)
+      case object
+      when QuantityField then object.mongoize
+      else object
+      end
+    end
   end
 end
--- spec/models/quantity_field_spec.rb
+++ spec/models/quantity_field_spec.rb
@@ -1,22 +1,22 @@
-require 'rails_helper'
-
 # frozen_string_literal: true
 
+require 'rails_helper'
+
 RSpec.describe QuantityField, type: :model do
-  QUANTITY_1CUPS = QuantityField.new(1, :cups)
-  QUANTITY_2CUPS = QuantityField.new(2, :cups)
-  QUANTITY_3CUPS = QuantityField.new(3, :cups)
-  QUANTITY_100GRAMS = QuantityField.new(100, :grams)
+  QUANTITY_1CUPS = QuantityField.new(1, 'cups')
+  QUANTITY_2CUPS = QuantityField.new(2, 'cups')
+  QUANTITY_3CUPS = QuantityField.new(3, 'cups')
+  QUANTITY_100GRAMS = QuantityField.new(100, 'grams')
 
   context 'Initialization' do
     it 'Values are properly assigned' do
       expect(QUANTITY_3CUPS).to_not be_nil
       expect(QUANTITY_3CUPS.amount).to be 3
-      expect(QUANTITY_3CUPS.unit).to be :cups
+      expect(QUANTITY_3CUPS.unit).to be 'cups'
     end
   end
 
-  context "simple manipulation" do
+  context 'simple manipulation' do
     it 'can be summed' do
       expect(QUANTITY_1CUPS + QUANTITY_2CUPS).to eq(QUANTITY_3CUPS)
     end
@@ -33,7 +33,18 @@ RSpec.describe QuantityField, type: :model do
 
   context 'when units are different' do
     it 'fails to sum' do
-      expect{ QUANTITY_1CUPS + QUANTITY_100GRAMS }.to raise_error
+      expect{ QUANTITY_1CUPS + QUANTITY_100GRAMS }.to raise_error(ArgumentError)
+    end
+  end
+
+  context 'database serialisation' do
+    it 'serializes into a compatible db object' do
+      expect(QUANTITY_1CUPS.mongoize).to eq([1, 'cups'])
+      expect(QuantityField.mongoize(QUANTITY_100GRAMS)).to eq([100, 'grams'])
+    end
+
+    it 'can be parsed from a mongo object' do
+      expect(QuantityField.demongoize([2, 'cups'])).to eq(QUANTITY_2CUPS)
     end
   end
 end
