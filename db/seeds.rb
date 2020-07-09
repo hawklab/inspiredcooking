58fc11e40c6b3a7960e0138ee8260e08a14e2fec
Carito <carito.orellana@gmail.com>
2020-07-09 23:49:27 +0000

    view details in show step

--- db/seeds.rb
+++ db/seeds.rb
@@ -71,15 +71,29 @@ Recipe.create({
   servings: "10",
   steps: [
     {
-      instructions: "Paso 1",
+      instructions: "Place the almond flour, coconut flour, sweetener, baking powder, and salt in a medium-large microwave-safe mug and blend with a fork.
+      Add the egg, butter, avocado oil, and vanilla; mix well.",
+      ingredients: [
+        {
+          id: "flour",
+          quantity: [500, "grams"]
+        },
+        {
+          id: "flour",
+          quantity: [500, "grams"]
+        },
+        {
+          id: "flour",
+          quantity: [500, "grams"]
+        }
+      ]
+    },
+    {
+      instructions: "Gently stir in the blueberries. Use the back of a spoon to press the batter down and smooth the top.",
       ingredients: []
     },
     {
-      instructions: "Paso 2",
-      ingredients: []
-    },
-    {
-      instructions: "Paso 3",
+      instructions: "Place the batter-filled mug in the microwave and heat for 1 minute 15 seconds. (The cooking time may vary depending on your microwave. If the muffin is not fully formed after 1 minute 15 seconds, continue cooking in 15-second increments.) Carefully remove the mug from the microwave—it will be hot—flip it upside down over a plate, and allow the muffin to slide out of the mug onto the plate. Place the muffin on its side and slice in half. Spread with butter, if desired, and enjoy!",
       ingredients: []
     }
   ],
