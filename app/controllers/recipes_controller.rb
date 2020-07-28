b0c1973b2c8bd0e1099baae6653fefe0256b11fa
Carito <carito.orellana@gmail.com>
2020-07-28 23:14:26 +0000

    favorite option for loggin user

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,23 +1,43 @@
 class RecipesController < ApplicationController
   # before_filter :authenticate_user!, :except => [:index, :show]
+
   def index
     @recipes = Recipe.all
   end
 
   def show
     @recipe = Recipe.find(params[:id])
+
+
+
   end
+
+
+
+
+
+
+
+
+
+
+
+
 
   def favorite
     @recipe = Recipe.find(params[:id])
+    if user_signed_in?
     @recipe.favorited_by = [current_user]
     redirect_to recipe_path(@recipe), notice:"You just add this recipe to your favorites"
+    else
+      redirect_to recipe_path(@recipe), notice:"If you want to add this recipe to your favorite recipies you need to be #{view_context.link_to "logged in", new_user_session_path}"
+    end
   end
 
   def unfavorite
     @recipe = Recipe.find(params[:id])
     @recipe.favorited_by = []
-    redirect_to recipe_path(@recipe), notice:"unfavorite"
+    redirect_to recipe_path(@recipe), notice: @recipe.name + " was remove from your favorites."
 
   end
 
