f802f2a9043a9037217f0d7267c9f5b744c6bc99
Carito <carito.orellana@gmail.com>
2020-07-29 21:59:00 +0000

    save favorites without deleting the list

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -12,22 +12,12 @@ class RecipesController < ApplicationController
 
   end
 
-
-
-
-
-
-
-
-
-
-
-
-
   def favorite
     @recipe = Recipe.find(params[:id])
     if user_signed_in?
-    @recipe.favorited_by = [current_user]
+    # @recipe.favorited_by = [current_user]
+    @recipe.push(favorited_by_ids: current_user.id)
+    current_user.push(favorite_recipe_ids: @recipe.id)
     redirect_to recipe_path(@recipe), notice:"You just add this recipe to your favorites"
     else
       redirect_to recipe_path(@recipe), notice:"If you want to add this recipe to your favorite recipies you need to be #{view_context.link_to "logged in", new_user_session_path}"
@@ -36,11 +26,11 @@ class RecipesController < ApplicationController
 
   def unfavorite
     @recipe = Recipe.find(params[:id])
-    @recipe.favorited_by = []
+    # @recipe.favorited_by = []
+    @recipe.pull(favorited_by_ids: current_user.id)
+    current_user.pull(favorite_recipe_ids: @recipe.id)
     redirect_to recipe_path(@recipe), notice: @recipe.name + " was remove from your favorites."
 
   end
-
-
 
 end
