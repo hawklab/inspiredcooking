c66f1f4bd8a3401da53865dec4b66c7d45eaa4a5
Carito Orellana <carito.orellana@gmail.com>
2021-02-17 17:50:00 -0800

    Connecting Entity Framework and SQL

---
+++ recipes.Data/RecipesDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using recipes.Core;
+
+namespace recipes.Data
+{
+    public class RecipesDbContext : DbContext
+    {
+        public RecipesDbContext(DbContextOptions<RecipesDbContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Recipe> Recipes { get; set; }
+    }
+}
