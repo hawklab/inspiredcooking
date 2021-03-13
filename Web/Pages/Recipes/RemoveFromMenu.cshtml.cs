6c35ef4d8859d567d3dfbf8c1aeed595a8c99209
Carito Orellana <carito.orellana@gmail.com>
2021-03-12 16:22:30 -0800

    Deleting recipes from menu

---
+++ Web/Pages/Recipes/RemoveFromMenu.cshtml.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using InspiredCooking.Web.Helpers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace InspiredCooking.Web.Pages.Recipes
+{
+    public class RemoveFromMenuModel : PageModel
+    {
+
+        public ActionResult OnGet(int recipeId)
+        {
+            var currentMenu = HttpContext.Session.GetObjectFromJson<List<int>>("CurrentMenu");
+
+            if (currentMenu == null)
+            {
+                currentMenu = new List<int>();
+            }
+            currentMenu.Remove(recipeId);
+            HttpContext.Session.SetObjectAsJson("CurrentMenu", currentMenu);
+
+
+            return Redirect(Request.Headers["Referer"].ToString());
+        }
+    }
+}
