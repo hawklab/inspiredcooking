a74bc1886a57cc62f8149da0480ea87010f4cc98
Seba Gamboa <me@sagmor.com>
2021-09-08 23:07:18 +0000

    more updates

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -3,4 +3,4 @@ pushd Web
 npm install
 popd
 
-dotnet ef database update --project Data -s Web
+dotnet ef database update --project Data -s Web --connection "Server=localhost;Database=RecipesDb;User Id=SA;Password=P@ssw0rd"
